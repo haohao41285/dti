542ee1061d5b56ba96b41a41271032bfa0aafb9f
lequochieu284 <lequochieu284@gmail.com>
2019-10-07 14:11:41 +0700

    fix edit profile

--- resources/views/logs/recent-logs.blade.php
+++ resources/views/logs/recent-logs.blade.php
@@ -14,7 +14,7 @@ Recent Logs
         <th>TIMESTAMP</th>            
         </tr>
         </thead>
-        <tbody>
+        {{-- <tbody>
             <tr role="row">
                 <td class=" alignCenter" tabindex="0">2821</td>
                 <td class=" alignCenter">linhhoang</td>
@@ -23,7 +23,7 @@ Recent Logs
                 <td class=" alignCenter">::1</td>
                 <td class="alignCenter sorting_1">24/07/2019 10:13:38 AM</td>
             </tr>
-        </tbody>
+        </tbody> --}}
     </table>
 </div>
 @endsection
@@ -32,19 +32,19 @@ Recent Logs
     $(document).ready(function () {
             table = $('#log-datatable').DataTable({
              // dom: "lBfrtip",
-          //    buttons: [
-          //     {
-          //         text: '<i class="fas fa-trash"></i> Clear All Logs',
-          //         className: 'btn btn-sm btn-primary',
-          //         action: function ( e, dt, node, config ) {
-          //            // document.location.href = "";
-          //         }
-          //     },
-          //     { text : '<i class="fas fa-download"></i> Export',
-          //       extend: 'csvHtml5', 
-          //       className: 'btn btn-sm btn-primary' 
-          //     }
-          // ],  
+             buttons: [
+              {
+                  text: '<i class="fas fa-trash"></i> Clear All Logs',
+                  className: 'btn btn-sm btn-primary',
+                  action: function ( e, dt, node, config ) {
+                     // document.location.href = "";
+                  }
+              },
+              { text : '<i class="fas fa-download"></i> Export',
+                extend: 'csvHtml5', 
+                className: 'btn btn-sm btn-primary' 
+              }
+          ],  
              processing: true,
              serverSide: true,
              ajax:{ url:"{{ route('recentlogDatatable') }}",
@@ -52,56 +52,17 @@ Recent Logs
 
                   } 
               },
-             mData: [
+             columns: [
 
                        { data: 'id', name: 'id' },
+                       { data: 'user_id', name: 'user_id' },
                        { data: 'type', name: 'type' },
                        { data: 'message', name: 'message' },
                        { data: 'ip_address', name: 'ip_address' },
                        { data: 'created_at', name: 'created_at' },
-                       { data: 'created_at', name: 'created_at' },
               ],       
            }); 
-      //   $('#log-datatable').DataTable({
-      //        buttons: [
-      //         {
-      //             text: '<i class="fas fa-trash"></i> Clear All Logs',
-      //             className: 'btn btn-sm btn-primary',
-      //             action: function ( e, dt, node, config ) {
-      //                document.location.href = "{{ route('addCombo') }}";
-      //             }
-      //         },
-      //         { text : '<i class="fas fa-download"></i> Export',
-      //           extend: 'csvHtml5', 
-      //           className: 'btn btn-sm btn-primary' 
-      //         }
-      //     ]  ,
-      // "ajax": {
-      //      "url": "{{ route('recentlogDatatable') }}",
-      //      "data": function ( d ) {
-      //          // d.status = $('#filterStatus').val();
-      //      }
-      //  },
-      //    columns:[
-      //        { data: 'id', name: 'id' },
-      //        { data: 'type', name: 'type' },
-      //        { data: 'message', name: 'message' },
-      //        { data: 'ip_address', name: 'ip_address' },
-      //        { data: 'created_at', name: 'created_at' },
-      //    ],
-      //   });
-        // var arrStatus = [
-        //     {val: "", text: '-- Event Type -- '},
-        //     {val: 1, text: 'LOGIN'},
-        //     {val: 2, text: 'ADD'},
-        //     {val: 3, text: 'EDIT'},
-        //     {val: 5, text: 'DELETE'},
-        // ];
-        // var statusFilter = $("<select id='filterStatus' class='custom-select custom-select-sm form-control form-control-sm'/>");
-        // $.each(arrStatus, function (i, item) {
-        //     statusFilter.append($("<option>").attr('value', item.val).text(item.text));
-        // });
-        // $('<label />').append(statusFilter).appendTo($("#dataTableAllCustomer_filter"));
+      
     });
 </script>
 @endpush
