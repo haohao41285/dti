575b62c0563b5adc9496ffbf276754888ecc274e
thieusumo <nguyenthieupro93@gmail.com>
2020-01-15 13:25:40 +0700

    add place

--- resources/views/tools/demo-place.blade.php
+++ resources/views/tools/demo-place.blade.php
@@ -10,6 +10,115 @@
         tbody tr .disabled{
             text-decoration: line-through solid red;
         }
+        .mycard-footer {
+    height: 25px;
+    background: #333333;
+    font-size: 15px;
+    text-indent: 10px;
+   /* border-radius: 0 0px 4px 4px;*/
+}
+
+.gallery-card {
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+        height: 132px;
+        margin-bottom:14px;
+}
+.gallery-card-body {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    /*padding: 1.25rem;*/
+}
+.gallery-card img {
+    height: 100px;
+    width: 100%;
+}
+label{
+    margin-bottom: 0 !important;
+}
+/*--checkbox--*/
+
+.block-check {
+    display: block;
+    position: relative;
+   
+   
+    cursor: pointer;
+    font-size: 22px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.block-check input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+    cursor: pointer;
+}
+
+/* On mouse-over, add a grey background color */
+.block-check:hover input ~ .checkmark {
+    background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.block-check input:checked ~ .checkmark {
+    background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+}
+
+/* Show the checkmark when checked */
+.block-check input:checked ~ .checkmark:after {
+    display: block;
+}
+
+/* Style the checkmark/indicator */
+.block-check .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+}
+    .card-link{
+        color: white;
+    }
     </style>
 @endpush
 @section('content')
@@ -47,21 +156,46 @@
                                     <h6 class="m-0 font-weight-bold text-primary">Infomation Customer</h6>
                             </div>
                             <form id="add-form">
-                                <div class="card-body">
-                                    <div class="col-12 row">
-                                            <div class="input-group mb-3 input-group-sm">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text">Customer Phone<sup class="text-danger">*</sup></span>
+                                <div class="col-12 row">
+                                        <div class="input-group mb-3 input-group-sm">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">Customer Phone<sup class="text-danger">*</sup></span>
+                                            </div>
+                                            <input type="text" onkeypress="return isNumberKey(event)" class="form-control customer_phone" name="customer_phone">
+                                        </div>
+                                        <span class="text-danger" style="font-size: 12px">Customer Phone is not with character "0" at the first</span>
+                                        <span class="text-danger" style="font-size: 12px">Customer Phone includes 10 characters</span>
+                                        <hr>
+                                        <div class="custom-control custom-checkbox mb-3">
+                                          <input type="checkbox" class="custom-control-input" id="place_demo" value="1" name="place_demo">
+                                          <label class="custom-control-label" for="place_demo">Place Demo</label>
+                                        </div>
+                                        <div class="col-md-12 border-1"></div>
+                                       {{--  <div class="theme_list">
+                                            @foreach($theme_list as $theme)
+                                                <img style='height: 4rem;' src='{{env('URL_FILE_VIEW').$theme->theme_image}}' alt=''>
+                                            @endforeach
+                                        </div> --}}
+                                        @foreach($theme_list as $theme)
+                                            <div class="col-md-3">
+                                              <div class="gallery-card">
+                                                <div class="gallery-card-body">
+                                                  <label class="block-check">
+                                                 <img src="{{env('URL_FILE_VIEW').$theme->theme_image}}" class="img-responsive" />
+                                                <input type="radio" name="theme_website" value="{{ $theme->theme_name_temp }}">
+                                                  <span class="checkmark"></span>
+                                                  </label>
+                                                   <div class="mycard-footer">
+                                                    <a href="" class="card-link">{{ $theme->theme_name }}</a>
+                                                    </div>
                                                 </div>
-                                                <input type="text" onkeypress="return isNumberKey(event)" class="form-control customer_phone" name="customer_phone">
+                                              </div>
                                             </div>
-                                            <span class="text-danger" style="font-size: 12px">Customer Phone is not with character "0" at the first</span>
-                                            <span class="text-danger" style="font-size: 12px">Customer Phone includes 10 characters</span>
-                                        <div class="col-12">
-                                            <div class="form-group float-right">
-                                                <input type="button" class="btn btn-sm btn-danger cancel" data-dismiss="modal" value="Cancel">
-                                                <input type="button" class="btn btn-sm btn-primary submit" value="Submit">
-                                            </div>
+                                        @endforeach
+                                    <div class="col-12">
+                                        <div class="form-group float-right">
+                                            <input type="button" class="btn btn-sm btn-danger cancel" data-dismiss="modal" value="Cancel">
+                                            <input type="button" class="btn btn-sm btn-primary submit" value="Submit">
                                         </div>
                                     </div>
                                 </div>
